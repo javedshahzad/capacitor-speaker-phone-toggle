e9d29090dcc4f0763c916e93d80414b837b16730
Muhammad Javed Shahzad <javedshahzadnns@gmail.com>
2025-03-12 11:48:04 +0500

    Ios implementation

--- ios/Sources/Plugin/AudioToggle.swift
+++ ios/Sources/Plugin/AudioToggle.swift
@@ -1,8 +1,35 @@
 import Foundation
+import Capacitor
+import Foundation
+import AVFoundation
 
 @objc public class AudioToggle: NSObject {
     @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+    let session: AVAudioSession = AVAudioSession.sharedInstance();
+        if (value == "false") {
+                do {
+                    print("AUDIO MODE: \(value)");
+                    try session.setCategory(.playAndRecord, mode: .voiceChat, options:[.interruptSpokenAudioAndMixWithOthers])
+                    try session.setActive(true);
+                    try session.overrideOutputAudioPort(.none);
+                    call.resolve([
+                        "mode":"EARPIECE"
+                    ]);
+                } catch let error as NSError {
+                    call.reject("ERROR \(value):  \(error.localizedDescription)")
+                }
+            } else {
+                do {
+                    print("AUDIO MODE: \(value)");
+                    try session.setCategory(.playAndRecord, mode: .voiceChat, options:[.interruptSpokenAudioAndMixWithOthers, .defaultToSpeaker]);
+                    try session.setActive(true);
+                    try session.overrideOutputAudioPort(.speaker);
+                  call.resolve([
+                        "mode":"SPEAKER"
+                    ]);
+                } catch let error as NSError {
+                    call.reject("ERROR \(value):  \(error.localizedDescription)");
+                }
+            }
     }
 }
